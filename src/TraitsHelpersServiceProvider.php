f7ba133deb9989afd77be06d1ad912273a1bb05c
Labrock <vladimirpeshkov5@yandex.ru>
2023-12-19 07:59:37 +0300

    initial commit

---
+++ src/TraitsHelpersServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Aweram\TraitsHelpers;
+
+use Illuminate\Support\ServiceProvider;
+class TraitsHelpersServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+
+    }
+
+    public function register(): void
+    {
+
+    }
+}
